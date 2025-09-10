77aa1fedca4b6ef7872cfec742554cdeb221d275
yogeshsharmavlogs <ys9659954@gmail.com>
2025-09-10 17:13:19 +0530

    use map fiunction.

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ alert("Not a big Problem")
 //arrays and objects
 
 var arr = [1, 2, 3, 4];
+
+// foreach map filter and indexOf
+arr.forEach(function(val){
+    console.log(val + '2');
+}) 
+
+
+// use map
+
+var array = [1, 2, 3, 4];
+
+var newarr = array.map(function(val){
+    return val*3;
+})
+console.log(newarr);
